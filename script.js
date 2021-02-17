8aa75acc6298d6fe270edaf2661775b07cf85cf5
Rosari Harlan <rosari.harlan@gmail.com>
2021-02-17 20:23:56 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let r = document.getElementById("red");
+let g = document.getElementById("green");
+let b = document.getElementById("blue");
+let hexCode = document.getElementById("hex");
+let hexColor = document.getElementById("hexColor");
+let redBg = document.getElementById("red-bg");
+let greenBg = document.getElementById("green-bg");
+let blueBg = document.getElementById("blue-bg");
+
+// function rgbToHex() {
+//   let red = parseInt(r.value).toString(16);
+//   let green = parseInt(g.value).toString(16);
+//   let blue = parseInt(b.value).toString(16);
+
+// }
